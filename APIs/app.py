4032ca9c02b1f482df8661e5721d5a45936fea50
sushant-soma <ksoe.codes@mail.com>
2023-09-04 18:54:35 +0530

    updated app.py

--- APIs/app.py
+++ APIs/app.py
@@ -2,8 +2,7 @@ from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
-# Create in-memory databases as dictionaries for departments and employees.
-# Each department has a unique department number (DNO) associated with it.
+# Create in-memory database as dictionaries
 departments = {
     10: 'Admin',
     20: 'Accounts',
@@ -12,8 +11,6 @@ departments = {
     50: 'Purchasing'
 }
 
-# Each employee has a unique employee number (ENO) and is associated with a department using DNO.
-# The 'SALARY' field represents the salary of the employee.
 employees = {
     1: {'ENO': 1, 'ENAME': 'Amal', 'DNO': 10, 'SALARY': 30000},
     2: {'ENO': 2, 'ENAME': 'Shyamal', 'DNO': 30, 'SALARY': 50000},
@@ -23,51 +20,33 @@ employees = {
     6: {'ENO': 6, 'ENAME': 'Parimal', 'DNO': 10, 'SALARY': 20000}
 }
 
-# API endpoint for retrieving employee details by ENO.
 @app.route('/api', methods=['GET'])
-def get_employee_by_eno_or_dname():
+def get_employee_by_eno():
     try:
-        # Extract the 'ENO' and 'DNAME' query parameters from the request URL.
-        eno = request.args.get('ENO')
-        dname = request.args.get('DNAME')
-        
-        if eno is not None:
-            # If 'ENO' is provided, attempt to convert it to an integer.
-            eno = int(eno)
-            
-            if eno in employees:
-                # If the employee exists, return their details as a JSON response.
-                return jsonify(employees[eno])
-            else:
-                # If the employee is not found, return a 404 response with a message.
-                return jsonify({'message': 'Employee not found'}), 404
-        
-        elif dname is not None:
-            # If 'DNAME' is provided, search for employees in the specified department.
-            matching_employees = []
-
-            for eno, employee in employees.items():
-                if departments.get(employee['DNO']) == dname:
-                    matching_employees.append(employee)
-
-            if matching_employees:
-                # If matching employees are found, return them as a JSON response.
-                return jsonify(matching_employees)
-            else:
-                # If no employees are found for the given department, return a 404 response with a message.
-                return jsonify({'message': 'No employees found for the given department'}), 404
-        
+        eno = int(request.args.get('ENO'))
+        if eno in employees:
+            return jsonify(employees[eno])
         else:
-            # If neither 'ENO' nor 'DNAME' is provided, return a 400 response with an error message.
-            return jsonify({'message': 'Invalid request'}), 400
-    
+            return jsonify({'message': 'Employee not found'}), 404
     except ValueError:
-        # Handle the case where 'ENO' is provided but is not a valid integer.
         return jsonify({'message': 'Invalid ENO format'}), 400
+
+@app.route('/api/employees_by_dname', methods=['GET'])
+def get_employees_by_dname():
+    try:
+        dname = request.args.get('DNAME')
+        matching_employees = []
+
+        for eno, employee in employees.items():
+            if departments.get(employee['DNO']) == dname:
+                matching_employees.append(employee)
+
+        if matching_employees:
+            return jsonify(matching_employees)
+        else:
+            return jsonify({'message': 'No employees found for the given department'}), 404
     except Exception as e:
-        # Handle unexpected errors and return a 500 response with an error message.
         return jsonify({'message': str(e)}), 500
 
-# Start the Flask application on port 9000.
 if __name__ == '__main__':
     app.run(port=9000)
