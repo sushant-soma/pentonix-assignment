d521b76bc32c0ccb1f5518e030114e1a62e0a6b6
sushant-soma <ksoe.codes@mail.com>
2023-09-05 11:40:19 +0530

    readme.md for longest substring

--- APIs/app.py
+++ APIs/app.py
@@ -20,32 +20,48 @@ employees = {
     6: {'ENO': 6, 'ENAME': 'Parimal', 'DNO': 10, 'SALARY': 20000}
 }
 
+# API endpoint for retrieving employee details by ENO or DNAME.
 @app.route('/api', methods=['GET'])
-def get_employee_by_eno():
+def get_employee_details():
     try:
-        eno = int(request.args.get('ENO'))
-        if eno in employees:
-            return jsonify(employees[eno])
-        else:
-            return jsonify({'message': 'Employee not found'}), 404
-    except ValueError:
-        return jsonify({'message': 'Invalid ENO format'}), 400
-
-@app.route('/api/employees_by_dname', methods=['GET'])
-def get_employees_by_dname():
-    try:
+        eno = request.args.get('ENO')
         dname = request.args.get('DNAME')
-        matching_employees = []
 
-        for eno, employee in employees.items():
-            if departments.get(employee['DNO']) == dname:
-                matching_employees.append(employee)
+        if eno is not None:
+            # If 'ENO' is provided, attempt to convert it to an integer.
+            eno = int(eno)
 
-        if matching_employees:
-            return jsonify(matching_employees)
+            if eno in employees:
+                # If the employee exists, return their details as a JSON response.
+                return jsonify(employees[eno])
+            else:
+                # If the employee is not found, return a 404 response with a message.
+                return jsonify({'message': 'Employee not found'}), 404
+
+        elif dname is not None:
+            # If 'DNAME' is provided, search for employees in the specified department.
+            matching_employees = []
+
+            for eno, employee in employees.items():
+                if departments.get(employee['DNO']) == dname:
+                    matching_employees.append(employee)
+
+            if matching_employees:
+                # If matching employees are found, return them as a JSON response.
+                return jsonify(matching_employees)
+            else:
+                # If no employees are found for the given department, return a 404 response with a message.
+                return jsonify({'message': 'No employees found for the given department'}), 404
+
         else:
-            return jsonify({'message': 'No employees found for the given department'}), 404
+            # If neither 'ENO' nor 'DNAME' is provided, return a 400 response with an error message.
+            return jsonify({'message': 'Invalid request'}), 400
+
+    except ValueError:
+        # Handle the case where 'ENO' is provided but is not a valid integer.
+        return jsonify({'message': 'Invalid ENO format'}), 400
     except Exception as e:
+        # Handle unexpected errors and return a 500 response with an error message.
         return jsonify({'message': str(e)}), 500
 
 if __name__ == '__main__':
